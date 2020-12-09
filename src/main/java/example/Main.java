d3865eb5c85e372b643957d03a8059263db0abce
Julian Lamprecht <51859473+Juli-EXP@users.noreply.github.com>
2020-12-09 18:57:10 +0100

    Initial commit, added an example to get the sysContact of localhost

---
+++ src/main/java/example/Main.java
@@ -0,0 +1,11 @@
+import org.snmp4j.smi.OID;
+
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        SimpleSnmpClient client = new SimpleSnmpClient("udp:127.0.0.1/161");
+        String sysDescr = client.getAsString(new OID(".1.3.6.1.2.1.1.4.0"));
+        System.out.println(sysDescr);
+    }
+}
